602fb2bc404788aca23abfc51c7e68d9817e82ca
chanotha <chanotha.dua@gmail.com>
2020-02-17 17:43:29 +0700

    get location realtime.

--- app/src/main/java/com/ballomo/electricdriver/MainActivity.kt
+++ app/src/main/java/com/ballomo/electricdriver/MainActivity.kt
@@ -1,17 +1,33 @@
 package com.ballomo.electricdriver
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.tasks.Tasks
 import com.google.firebase.firestore.FirebaseFirestore
+import io.nlopez.smartlocation.OnLocationUpdatedListener
+import io.nlopez.smartlocation.SmartLocation
+import io.nlopez.smartlocation.location.config.LocationAccuracy
+import io.nlopez.smartlocation.location.config.LocationParams
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
+import permissions.dispatcher.NeedsPermission
+import permissions.dispatcher.RuntimePermissions
 import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity(), CoroutineScope, OnMapReadyCallback {
+private const val REQUEST_ACCESS_FINE_LOCATION = 0
+@RuntimePermissions
+class MainActivity : AppCompatActivity(), CoroutineScope, OnMapReadyCallback,
+    OnLocationUpdatedListener {
+
+    private var mMap: GoogleMap? = null
 
     private val job = Job()
 
@@ -38,6 +54,8 @@ class MainActivity : AppCompatActivity(), CoroutineScope, OnMapReadyCallback {
         mapFragment.apply {
             getMapAsync(this@MainActivity)
         }
+
+        showLocation.text = "Test"
 
         launch {
             addUser()
@@ -73,7 +91,73 @@ class MainActivity : AppCompatActivity(), CoroutineScope, OnMapReadyCallback {
     }
 
     override fun onMapReady(map: GoogleMap?) {
+        map?.let {
+            mMap = it
+        }
+    }
 
+
+    @NeedsPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+    fun getCurrentLocation() {
+        setupLocationProvider()
+    }
+
+
+    private fun setupLocationProvider() {
+        val params = LocationParams.Builder()
+            .setAccuracy(LocationAccuracy.HIGH)
+            .setInterval(5000)
+            .build()
+
+        if (SmartLocation.with(this).location().state().locationServicesEnabled()) {
+            SmartLocation.with(this)
+                .location()
+                .config(params)
+                .start(this)
+        } else {
+            Toast.makeText(this, "Location Service Not Enable", Toast.LENGTH_LONG).show()
+        }
+    }
+
+
+    @SuppressLint("SetTextI18n")
+    override fun onLocationUpdated(location: Location?) {
+        location?.let {
+            val latitude = it.latitude
+            val longitude = it.longitude
+            val accuracy = it.accuracy
+            val bearing = it.bearing
+            val provider = it.provider
+
+            showLocation.text = "Location Update -> $latitude, $longitude, $accuracy, $bearing, $provider"
+            Log.d("Location", "Location Update -> $latitude, $longitude, $accuracy, $bearing, $provider")
+            return@let
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+
+        when(requestCode) {
+            REQUEST_ACCESS_FINE_LOCATION -> {
+                setupLocationProvider()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        getCurrentLocationWithPermissionCheck()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        SmartLocation.with(this)
+            .location()
+            .stop()
     }
 
     override fun onDestroy() {
